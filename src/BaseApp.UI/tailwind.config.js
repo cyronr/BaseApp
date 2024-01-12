06bd27e2b2016f5b3a12de94863dc968ebe11cb2
Remigiusz Cyron <remigiusz.cyron@gmail.com>
2024-01-12 18:26:20 +0100

    Styling login page

--- src/BaseApp.UI/tailwind.config.js
+++ src/BaseApp.UI/tailwind.config.js
@@ -24,7 +24,19 @@ module.exports = {
               'light-gray-800': '#98aeb3',
               'light-gray-900': '#8aa2a8',
 
-              'brigh-green-100': '#2ecc71'
+              'bright-green-100': '#6ede9c',
+              'bright-green-200': '#59d98e',
+              'bright-green-300': '#44d580',
+              'bright-green-400': '#2fd072',
+              'bright-green-500': '#2ecc71',
+              'bright-green-600': '#2abb67',
+              'bright-green-700': '#26a65b',
+              'bright-green-800': '#219150',
+              'bright-green-900': '#1c7d44'
+          },
+          width: {
+            '128': '32rem',
+            '160': '40rem'
           }
       },
   },
